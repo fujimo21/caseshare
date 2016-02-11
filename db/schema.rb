ae009950d6155deff7f9e2bdf8f8cd939a58a16e
fujimo21 <y.fujomoto@gmail.com>
2016-02-11 14:13:48 +0000

    user info/edit page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210142646) do
+ActiveRecord::Schema.define(version: 20160211135051) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20160210142646) do
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
+    t.string   "phone_number"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
