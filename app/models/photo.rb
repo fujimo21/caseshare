ea702dd4503001958effecbfea712a3710b11187
fujimo21 <y.fujomoto@gmail.com>
2016-02-21 12:58:53 +0000

    ver0.9341

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :suitcase
   
   if Rails.env.production?
-    has_attached_file :image, storage: :s3, s3_credentials: "caseshare/config/s3.yml", 
+    has_attached_file :image, storage: :s3, s3_credentials: "#{Rails.root}/config/s3.yml", 
                               styles: { medium: "300x300>", thumb: "100x100>" },url:":s3_domain_url",
                               path: ":image/:id/:style.:extension"
     validates_attachment_content_type :image, content_type: ["image/jpg", "image/JPG", "image/jpeg", "image/png", "image/gif"]
