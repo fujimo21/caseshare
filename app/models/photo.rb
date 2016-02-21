15eb93fd60bfe8097e4ddd354033b93cb7770047
fujimo21 <y.fujomoto@gmail.com>
2016-02-21 12:52:15 +0000

    ver 0.934

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,11 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :suitcase
   
   if Rails.env.production?
-    S3_CREDENTIALS={access_key_id:ENV['S3_ACCESS_KEY_ID'], secret_access_key:ENV['S3_SECRET_KEY'], bucket:"caseshare"}
-  end
-  
-  if Rails.env.production?
-    has_attached_file :image, storage: :s3, s3_credentials: S3_CREDENTIALS, 
+    has_attached_file :image, storage: :s3, s3_credentials: "caseshare/config/s3.yml", 
                               styles: { medium: "300x300>", thumb: "100x100>" },url:":s3_domain_url",
                               path: ":image/:id/:style.:extension"
     validates_attachment_content_type :image, content_type: ["image/jpg", "image/JPG", "image/jpeg", "image/png", "image/gif"]
