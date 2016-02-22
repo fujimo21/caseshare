a0ddfafddc69d04470b7593b4a026349ff61d8f1
fujimo21 <y.fujomoto@gmail.com>
2016-02-22 14:39:32 +0000

    ver 9.35

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'cloudinary'
 
 gem 'aws-sdk', '< 2.0'
 
+gem "font-awesome-rails"
+
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui/datepicker
+//= require jquery-ui/datepicker-ja
 //= require turbolinks
 //= require toastr
 //= require private_pub
